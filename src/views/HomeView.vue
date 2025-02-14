1c86b0e8a0f99e560b9242ca7406aa253aa35c04
George Stones <george@MacBook-Air-George-2.local>
2025-02-14 17:38:56 +0300

    added first checkbox

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="home">
-    <H1 class="home__header">{{ a }}</H1>
+    <div class="home__line">
+      <input class="home__check" type="checkbox" aria-label="done or not"/>
+      <H1 class="home__header">
+        {{ a }}
+      </H1>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
   .home {
     margin: 5%;
-    height: calc(90vh - 3em);
     background-color: greenyellow;
+
+    &__line {
+      display: flex;
+      align-items: center;
+      // justify-content: center;
+    }
+
+    &__check {
+      appearance: none;
+      width: 1.9em;
+      height: 1.9em;
+      margin-right: 1em;
+      border-radius: 50%;
+      border: 1.5px solid #333;
+      background-color: white;
+      cursor: pointer;
+      display: inline-block;
+    }
+
     &__header {
       margin: 0;
       background-color: rgb(255, 98, 179);
+      min-height: 1em;
     }
   }
 </style>
