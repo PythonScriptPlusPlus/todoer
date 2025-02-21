872f83143dd598e15b3e067176ece0f989f6e4d1
George Stones <george@MacBook-Air-George-2.local>
2025-02-21 15:50:09 +0300

    stylized checkmark + text crosses out if marked as done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <div class="home__line">
-      <input class="home__check" type="checkbox" aria-label="done or not"/>
-      <H1 class="home__header">
+      <input class="home__check" type="checkbox" aria-label="done or not" v-model="isChecked"/>
+      <H1 class="home__header" :class="isChecked ? 'home__header--crossed' : ''">
         {{ a }}
       </H1>
     </div>
@@ -11,8 +11,8 @@
 
 <style lang="scss">
   .home {
-    margin: 5%;
-    background-color: greenyellow;
+    margin: 2em;
+    // background-color: greenyellow;
 
     &__line {
       display: flex;
@@ -21,21 +21,49 @@
     }
 
     &__check {
+      // transition: all 0.5s;
       appearance: none;
       width: 1.9em;
       height: 1.9em;
-      margin-right: 1em;
+      margin: 0 2em 0 0.1em;
       border-radius: 50%;
       border: 1.5px solid #333;
       background-color: white;
       cursor: pointer;
       display: inline-block;
+      position: relative;
+
+      &::after {
+        content: '✔';
+        font-size: 1.2em;
+        color: white;
+        font-weight: bold;
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 0;
+        // transition: opacity 0.3s ease, transform 0.3s ease;
+      }
+
+      &:checked {
+        background-color: #333;
+
+        &::after {
+          opacity: 1;
+        }
+      }
     }
 
     &__header {
       margin: 0;
-      background-color: rgb(255, 98, 179);
       min-height: 1em;
+      text-align: left;
+      width: 90%;
+
+      &--crossed {
+        text-decoration: line-through;
+      }
     }
   }
 </style>
@@ -44,7 +72,8 @@
 export default {
   data() {
     return {
-      a: 'Hell',
+      isChecked: true,
+      a: 'Ullamco excepteur dolor in non mollit irure eu aute consectetur consectetur sunt velit.',
     };
   },
 };
